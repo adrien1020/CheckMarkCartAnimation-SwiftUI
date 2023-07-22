c7fe56e1bc5ece1742f8ad7238486c23e18cd2f0
Adrien SURUGUE <adrien.surugue@gmail.com>
2023-07-22 13:40:30 +0200

    Initial Commit

---
+++ CheckmarkCart/CheckmarkCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckmarkCartApp.swift
+//  CheckmarkCart
+//
+//  Created by Adrien Surugue on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckmarkCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
